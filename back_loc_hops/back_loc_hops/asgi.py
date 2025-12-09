2671ee6f5c7bfe77f81e4255f30a26bad39877f8
Lala Diallo <diallo.lala@ugb.edu.sn>
2025-12-09 20:29:01 +0100

    ajout du backend

---
+++ back_loc_hops/back_loc_hops/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for back_loc_hops project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/6.0/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'back_loc_hops.settings')
+
+application = get_asgi_application()
